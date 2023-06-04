6e76b828f48fab951b315a991574ff38b6c9f4d2
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-06-04 17:42:43 +0300

    Changed monkeyservice

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,36 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+
+            //    using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+            //    using var reader = new StreamReader(stream);
+            //    var contents = await reader.ReadToEndAsync();
+            //    monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);
+        }
+
+        return monkeyList;
+
+    }
 }
