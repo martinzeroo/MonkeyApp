f6afecd11d5fc9d611d7bad13128114fbc44ffc4
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-06-04 17:37:32 +0300

    Changed mainpage and baseviewmodel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,42 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public class BaseViewModel : INotifyPropertyChanged
 {
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsNotBusy => !isBusy;
+
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+    public void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
