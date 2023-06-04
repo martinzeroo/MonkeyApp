a07ae6c80af184e406dc6cf0169ca4312327159a
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-06-04 17:41:51 +0300

    Made baseviewmodel better

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,42 +1,13 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
+
+    [ObservableProperty]
     string title;
 
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-                return;
-            isBusy = value;
-            OnPropertyChanged();
-        }
-    }
-
-    public bool IsNotBusy => !isBusy;
-
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value)
-                return;
-            title = value;
-            OnPropertyChanged();
-        }
-    }
-    public event PropertyChangedEventHandler PropertyChanged;
-
-
-
-    public void OnPropertyChanged([CallerMemberName] string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
+    public bool IsNotBusy => !IsBusy;
 }
