4c37c29c1306927ed8890cbd7f02337c6e2231b3
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-06-04 17:46:06 +0300

    Changed monkeyviewmodel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,51 @@
 ﻿namespace MonkeyFinder.ViewModel;
+using MonkeyFinder.Services;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    MonkeyService monkeyService;
+
+    public Command GetMonkeysCommand { get; }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+
+    }
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+        //GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
+    }
+
 }
