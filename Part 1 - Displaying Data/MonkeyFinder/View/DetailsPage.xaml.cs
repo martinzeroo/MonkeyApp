b3b6051c68e994995c5ee91de5e4e52982caa05d
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-06-04 18:07:20 +0300

    Changed detailspage and monkeydetailsviewmodel

--- Part 1 - Displaying Data/MonkeyFinder/View/DetailsPage.xaml.cs
+++ Part 1 - Displaying Data/MonkeyFinder/View/DetailsPage.xaml.cs
@@ -1,9 +1,14 @@
 namespace MonkeyFinder;
 
+//[QueryProperty("Monkey", "Monkey")]
 public partial class DetailsPage : ContentPage
 {
-	public DetailsPage()
-	{
-		InitializeComponent();
-	}
+
+    //public Monkey Monkey { get; set; }
+    public DetailsPage(MonkeyDetailsViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+
+    }
 }
